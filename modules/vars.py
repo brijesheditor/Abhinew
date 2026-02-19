6bbd3a08083fb2f5e75218707b18389075aa7741
𝄟✮͢🦋⃟≛⃝𝑩𝕣ΐ𝕛℮𝚜Ꮒ✮⃝🩷 <brijesheditorhtw@gmail.com>
2026-02-19 15:05:20 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,18 +3,18 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "26184715"))
-API_HASH = environ.get("API_HASH", "7fc42fe25c89660b4e0b00dd7aa0beb1")
-BOT_TOKEN = environ.get("BOT_TOKEN", "8521628886:AAHKpyhbHzKEB9MEJMi-ZctnX32XBWeAE68")
+API_ID = int(environ.get("API_ID", "26421834"))
+API_HASH = environ.get("API_HASH", " fe21db9f61ff01300fc1fa3673617983")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7799051010:AAEuIuyW1ocE8PsdtbiCag9YO190IsYeKVE")
 
-OWNER = int(environ.get("OWNER", "7744686564"))
-CREDIT = environ.get("CREDIT", "♻️GABBAR♻️")
+OWNER = int(environ.get("OWNER", "6344648593"))
+CREDIT = environ.get("CREDIT", "♻️𝐁𝐑𝐈𝐉𝐄𝐒𝐇♻️")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
-TOTAL_USER = os.environ.get('TOTAL_USERS', '5680454765').split(',')
+TOTAL_USER = os.environ.get('TOTAL_USERS', '8584224126').split(',')
 TOTAL_USERS = [int(user_id) for user_id in TOTAL_USER]
 
-AUTH_USER = os.environ.get('AUTH_USERS', '5680454765').split(',')
+AUTH_USER = os.environ.get('AUTH_USERS', '8584224126').split(',')
 AUTH_USERS = [int(user_id) for user_id in AUTH_USER]
 if int(OWNER) not in AUTH_USERS:
     AUTH_USERS.append(int(OWNER))
@@ -23,6 +23,7 @@ if int(OWNER) not in AUTH_USERS:
 api_url = "http://master-api-v3.vercel.app/"
 api_token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoiNzkxOTMzNDE5NSIsInRnX3VzZXJuYW1lIjoi4p61IFtvZmZsaW5lXSIsImlhdCI6MTczODY5MjA3N30.SXzZ1MZcvMp5sGESj0hBKSghhxJ3k1GTWoBUbivUe1I"
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
 
 
 
